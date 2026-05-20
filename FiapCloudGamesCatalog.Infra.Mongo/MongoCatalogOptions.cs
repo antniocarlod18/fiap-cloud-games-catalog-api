14f8627196c480fc7ef8926f508028bb46f50715
DESKTOP-M76EG70\antni <antniocarlod@outlook.com>
2026-05-20 08:48:12 -0300

    Remove projeto não usado

--- FiapCloudGamesCatalog.Infra.Mongo/MongoCatalogOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace FiapCloudGamesCatalog.Infra.Mongo;
-
-public class MongoCatalogOptions
-{
-    public const string SectionName = "MongoCatalog";
-
-    public string ConnectionString { get; set; } = string.Empty;
-    public string DatabaseName { get; set; } = "catalog_mongo";
-}
--- FiapCloudGamesCatalog.Infra.Mongo/MongoCollectionNames.cs
+++
@@ -1,7 +0,0 @@
-namespace FiapCloudGamesCatalog.Infra.Mongo;
-
-public static class MongoCollectionNames
-{
-    public const string StoredEvents = "stored_events";
-    public const string GameReviews = "game_reviews";
-}
